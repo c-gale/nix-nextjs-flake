312706acd917d4b75db0295706911899f900c233
c-gale <cubylive@gmail.com>
2025-11-27 14:54:38 -0700

    Change flake a lil bit

--- flake.nix
+++ flake.nix
@@ -7,25 +7,20 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_24
-            nodePackages_latest.pnpm
-            nodePackages_latest.vercel
-            nodePackages_latest.prisma
-            postgresql_15
-            openssl
+    packages = flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.buildEnv {
+          name = "nextjs-env";
+          paths = [
+            pkgs.nodejs_24
+            pkgs.nodePackages_latest.pnpm
+            pkgs.nodePackages_latest.vercel
+            pkgs.nodePackages_latest.prisma
+            pkgs.postgresql_15
+            pkgs.openssl
           ];
-          shellHook = ''
-            export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
-            export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
-            export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
-          '';
         };
-      });
+    });
+
 }
