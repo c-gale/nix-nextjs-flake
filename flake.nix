b47b31cf31cc589994a06656f147d633fb89d261
c-gale <cubylive@gmail.com>
2025-11-27 15:06:10 -0700

    Added devshells

--- flake.nix
+++ flake.nix
@@ -12,6 +12,26 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        devShells.default = pkgs.mkShell {
+          name = "nextjs-dev-shell";
+
+          buildInputs = with pkgs; [
+            nodejs_24
+            nodePackages_latest.pnpm
+            nodePackages_latest.vercel
+            nodePackages_latest.prisma
+            postgresql_15
+            openssl
+            prisma-engines  # ensures availability for shellHook
+          ];
+
+          shellHook = ''
+            export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
+            export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
+            export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
+          '';
+        };
+
         packages.default = pkgs.buildEnv {
           name = "nextjs-env";
           paths = [
