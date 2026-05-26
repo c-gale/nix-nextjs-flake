18328f47b886482c3c5dd116afe9dc6bfca78f40
c-gale <91436354+c-gale@users.noreply.github.com>
2026-05-25 23:09:05 -0600

    Fix path againnn

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
           ];
 
           shellHook = ''
-            export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/bin/libquery_engine.node
+            export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
             export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
             export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
           '';
